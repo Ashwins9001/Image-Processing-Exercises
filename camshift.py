f99f564ac8ef3d4d14c0058450ba8a6dd05101ea
ashwins9001 <nitromekid@hotmail.com>
2019-11-04 15:51:30 -0500

    KLT using Pyramids
    Improved optical flow using KLT and pyramids for faster,  greater movement of object. Click to select feature to track

--- camshift.py
+++ camshift.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-
 video = cv2.VideoCapture("bath_mouse.mp4")
 _, first_frame = video.read()
 x = 200
@@ -12,27 +11,23 @@ hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
 roi_hist = cv2.calcHist([hsv_roi], [0], None, [180], [0, 180])
 roi_hist = cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
 term_criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
-cap = cv2.VideoCapture(0)
 
 while True:
-    _, frame = cap.read()
+    _, frame = video.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
-    #kernel = np.ones((5,5),np.uint8)
-    #mask = cv2.dilate(mask,kernel,iterations = 1)
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
     mask = cv2.filter2D(mask, -1, kernel)
     
-    _, track_window = cv2.meanShift(mask, (x, y, width, height), term_criteria)
-    x, y, w, h = track_window
-    cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 20)
+    ret, track_window = cv2.CamShift(mask, (x, y, width, height), term_criteria)
+    pts = cv2.boxPoints(ret)
+    pts = np.int0(pts)
+    cv2.polylines(frame, [pts], True, (255,0,0), 2)
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
     
-    #Can increase refresh rate to improve accuracy by lowering mask distortion
     key = cv2.waitKey(60)
     if key == 27:
         break
 video.release()
-cap.release()
 cv2.destroyAllWindows()
